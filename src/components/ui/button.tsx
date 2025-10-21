12399aa4e5272d354de8cf1c86b7548b391174c1
Pierre Humblot-Ferrero <phf@grand-shooting.com>
2025-10-21 15:07:30 +0200

    fix: Ajout padding horizontal px-4 pour Button size=large

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -76,8 +76,8 @@ const buttonVariants = cva(
         link: "btn-link",
       },
       size: {
-        small: "p-1 text-sm gap-1",  // 5px padding, gap 5px, font 11px
-        large: "p-1 text-base gap-2", // 5px padding, gap 10px, font 13px
+        small: "p-1 text-sm gap-1",        // 5px padding, gap 5px, font 11px
+        large: "px-4 py-1 text-base gap-2", // 20px horizontal, 5px vertical, gap 10px, font 13px
       },
     },
     defaultVariants: {
